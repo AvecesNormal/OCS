b973ebcafe720e0de0fec35d88d5180dbc4c966f
javier <javierpardollanes@gmail.com>
2024-08-05 05:51:23 -0500

    estilos y logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-      Hola mundo
+      <div className="bg-slate-800 min-h-screen">
+        <h1 className="text-3xl font-bold underline">
+        Hola mundo 
+        </h1>
       </div>
     </>
   )
